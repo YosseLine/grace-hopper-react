5229b4321500bd048ce9073d5eecb42c9f74b262
YosseLine <yoseline_96@hotmail.com>
2018-03-03 22:31:30 -0500

    Inicializando proyecto y subiendo estructura y maquetado básico

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Box-header">
+        <h1 className="Header-title">Grace Hopper</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
